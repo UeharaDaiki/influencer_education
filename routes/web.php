cac25beb13ced72b72d374e659b559d5a1fc6f10
UeharaDaiki <u.daiki0122@gmail.com>
2025-04-08 15:44:22 +0900

    GradeTitles

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,13 @@ Route::prefix('user')->namespace('Users')->name('user.')->group(function () {
     Route::get('/top', [App\Http\Controllers\Users\TopController::class, 'showTop'])->name('show.top');
     Route::get('/curriculum_list', [App\Http\Controllers\Users\CurriculumController::class, 'showCurriculum'])->name('show.curriculum');
     Route::get('/profile', [App\Http\Controllers\Users\ProfileController::class, 'showProfile'])->name('show.profile');
-    Route::get('/login', [App\Http\Controllers\Users\LoginController::class, 'showLogin'])->name('show.login');    
+    Route::get('/login', [App\Http\Controllers\Users\LoginController::class, 'showLogin'])->name('show.login');
+    Route::get('/article/{id}', [App\Http\Controllers\Users\ArticleController::class, 'showArticle'])->name('show.article');
+
+});
+Route::prefix('admin')->namespace('Admin')->name('admin.')->group(function () {
+    Route::get('/article_list', [App\Http\Controllers\Admin\ArticleController::class, 'showArticleList'])->name('show.article.list');
+    Route::get('/article_create', [App\Http\Controllers\Admin\ArticleController::class, 'showArticleCreate'])->name('show.article.create');
+    Route::get('/article_edit/{id}', [App\Http\Controllers\Admin\ArticleController::class, 'showArticleEdit'])->name('show.article.edit');
+    Route::post('/article_delete/{id}', [App\Http\Controllers\Admin\ArticleController::class, 'showArticleDelete'])->name('show.article.delete');
 });
