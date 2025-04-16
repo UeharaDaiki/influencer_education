f2466b8550d7909b90e9f4db339b7976f124b330
hono21 <0521hono@gmail.com>
2025-04-16 14:57:48 +0900

    HTMLのみ仮コーディング

---
+++ resources/views/user/curriculum_list.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>時間割</title>
+    </head>
+    <body>
+        <header>
+            <div>
+                <button>時間割</button>
+                <button>授業進捗</button>
+                <button>プロフィール設定</button>
+                <a href="#">ログアウト</a>
+            </div>
+        </header>
+
+        <div>
+            <a href="#">戻る</a>
+            <span>2025年4月スケジュール</span>
+            <span>小学校1年生</span>
+        </div>
+
+        <div>
+            <aside>
+                <ul>
+                    <li>小学校1年生</li>
+                    <li>小学校2年生</li>
+                    <li>小学校3年生</li>
+                    <li>小学校4年生</li>
+                    <li>小学校5年生</li>
+                    <li>小学校6年生</li>
+                    <li>中学校1年生</li>
+                    <li>中学校2年生</li>
+                    <li>中学校3年生</li>
+                    <li>高校1年生</li>
+                    <li>高校2年生</li>
+                    <li>高校3年生</li>
+                </ul>
+            </aside>
+            <main>
+                <div>
+                    <img src="#" alt="サムネイル">
+                    <h2>授業タイトル</h2>
+                    <ul>
+                        <li>4月15日 14:00~15:00</li>
+                        <li>4月15日 14:00~15:00</li>
+                        <li>4月15日 14:00~15:00</li>
+                    </ul>
+                </div>
+            </main>
+        </div>
+        
+    </body>
+</html>
