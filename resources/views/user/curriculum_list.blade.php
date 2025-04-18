e3ae7a3d3163da9d22000bc248d6ac0cc7a4e304
hono21 <0521hono@gmail.com>
2025-04-18 09:37:17 +0900

    学年ボタンをリスト状態から修正

--- resources/views/user/curriculum_list.blade.php
+++ resources/views/user/curriculum_list.blade.php
@@ -27,20 +27,18 @@
 
         <div>
             <aside>
-                <ul>
-                    <li><button>小学校1年生</button></li>
-                    <li><button>小学校2年生</button></li>
-                    <li><button>小学校3年生</button></li>
-                    <li><button>小学校4年生</button></li>
-                    <li><button>小学校5年生</button></li>
-                    <li><button>小学校6年生</button></li>
-                    <li><button>中学校1年生</button></li>
-                    <li><button>中学校2年生</button></li>
-                    <li><button>中学校3年生</button></li>
-                    <li><button>高校1年生</button></li>
-                    <li><button>高校2年生</button></li>
-                    <li><button>高校3年生</button></li>
-                </ul>
+                <button>小学校1年生</button><br>
+                <button>小学校2年生</button><br>
+                <button>小学校3年生</button><br>
+                <button>小学校4年生</button><br>
+                <button>小学校5年生</button><br>
+                <button>小学校6年生</button><br>
+                <button>中学校1年生</button><br>
+                <button>中学校2年生</button><br>
+                <button>中学校3年生</button><br>
+                <button>高校1年生</button><br>
+                <button>高校2年生</button><br>
+                <button>高校3年生</button>
             </aside>
             <main>
                 <div>
